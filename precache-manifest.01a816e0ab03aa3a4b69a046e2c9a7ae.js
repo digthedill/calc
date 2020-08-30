0298e9f483c022da93fe9ecab5aedfc42e646435
digthedill <popssly@gmail.com>
2020-08-30 12:59:15 -0500

    Updates

---
+++ precache-manifest.01a816e0ab03aa3a4b69a046e2c9a7ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174adec4b420df84a5022915d325f117",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "e3c669ec4e03c0f2ab8a",
+    "url": "/calc/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "5146d7967c72f22de44d",
+    "url": "/calc/static/css/main.61a1d0ea.chunk.css"
+  },
+  {
+    "revision": "e3c669ec4e03c0f2ab8a",
+    "url": "/calc/static/js/2.301e2d72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc/static/js/2.301e2d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5146d7967c72f22de44d",
+    "url": "/calc/static/js/main.9a511e13.chunk.js"
+  },
+  {
+    "revision": "3eefc945a4aed9ab2cd7",
+    "url": "/calc/static/js/runtime-main.0a4af9a5.js"
+  }
+]);
